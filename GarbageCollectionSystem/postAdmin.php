7d9e3147293cf48d22f858a822ef4686780110c4
IROdEvO <asiriiroshan@hotmail.com>
2018-12-30 17:49:04 +0530

    Little improvement

--- GarbageCollectionSystem/postAdmin.php
+++ GarbageCollectionSystem/postAdmin.php
@@ -19,7 +19,7 @@ include('connection.php');
 <div class="navigationbar">
   <a href="WelcomePageAdmin.php">Welcome</a>
   <a class="active" href="PostsPageAdmin.php">Posts</a>
-  <a href="ContactPageAdmin.php">Contact Us</a>
+  
   <a href="#about">About Us</a>
   <div class="profileMenu">
     <button class="profileButton"><i class="fa fa-bars"></i></button>
